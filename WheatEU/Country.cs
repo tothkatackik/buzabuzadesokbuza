92a2d7fa77a36b0915c8ee5b5dc267edaeca4a29
Tóth Kata <toth.kata@ckik.hu>
2026-03-11 12:30:56 +0100

    AAAAAA

---
+++ WheatEU/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheatEU
+{
+    internal class Country
+    {
+        private string name;
+        private Dictionary<int, string> wheatAmount;
+
+        public Country(string name, Dictionary<int, string> wheatAmount)
+        {
+            this.name = name;
+            this.wheatAmount = wheatAmount;
+        }
+    }
+}
--- WheatEU/Form1.cs
+++ WheatEU/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,22 @@ namespace WheatEU
 
         private void OpenMenuItem_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel fájl|*.csv|Minden fájl|*.*";
+            ofd.InitialDirectory = Application.StartupPath;
+            DialogResult result = ofd.ShowDialog();
+            if (result != DialogResult.OK) return;
+            using (StreamReader sr = new StreamReader(ofd.FileName))
+            {
+                string[] firstLine = sr.ReadLine().Split(';');
+                firstLine.Skip(1);
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+                    Dictionary<string, int> helpme = new Dictionary<string, int>();
+                    
+                }
+            }
         }
     }
 }
